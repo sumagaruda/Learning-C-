8978fe41ceb94cbd13a7c3d991b2e289d5959057
sumagaruda <35711129+sumagaruda@users.noreply.github.com>
2019-02-24 23:18:14 -0600

    Add files via upload

---
+++ Day 2/pattern-alphabets.cpp
@@ -0,0 +1,37 @@
+#pattern
+
+#eg. for 5
+# ABCDEEDCBA
+# ABCDDCBA
+# ABCCBA
+# ABBA
+# AA
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter value for pattern" << endl;
+    int n;
+    cin >> n;
+
+    for (int i=0; i<n; i++)
+    {
+        char ch ='A';
+        for (int j=0; j<n-i; j++)
+        {
+            cout << ch;
+            ch = ch+1;
+        }
+        
+        ch = ch-1;
+        for (int j=0; j<n-i; j++)
+        {
+            cout << ch;
+            ch = ch-1;
+        }
+        cout << '\n';
+    }
+    return 0;
+}
---
+++ Day 2/pattern_0and1.cpp
@@ -0,0 +1,26 @@
+#make pattern according to input
+
+#1
+#01
+#101
+#0101
+#10101
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i, j, n, out;
+    cin >> n;
+    
+    for (i=1; i<=n; i++)
+    {
+        for (j=0; j<i; j++)
+        {
+            out = (i+j)&1;
+            cout << out;
+        }
+        cout << endl;
+    }
+}
